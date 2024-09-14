9733984297ff947daa44c3a7256778b38f8bbb14
jling97 <linyunini@gmail.com>
2024-09-14 13:08:40 +0800

    feat: add listing page

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const {engine} = require('express-handlebars')
 const app = express()
 const port = 3000
+const restaurants = require('./public/jsons/restaurant.json').results
+
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 
 app.use(express.static('public'))
 
@@ -9,10 +15,10 @@ app.get('/', (req,res) => {
 })
 
 app.get('/restaurants', (req,res) => {
-  res.send('listing restaurants')
+  res.render('index',{restaurants})
 })
 
-app.get('/restaurant/:id', (req,res) => {
+app.get('/restaurants/:id', (req,res) => {
   const id = req.params.id
   res.send(`read restaurant: ${id}`)
 })
